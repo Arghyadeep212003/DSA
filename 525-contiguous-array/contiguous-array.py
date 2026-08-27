d654892cb2b2efbcd6668f70e4620a5ef8d24c58
Arghyadeep Deb <debarghyadeep4@gmail.com>
2026-08-27 11:26:07 +0530

    Time: 123 ms (18.89%) | Memory: 17 MB (9.90%) - LeetSync

---
+++ 525-contiguous-array/contiguous-array.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def findMaxLength(self, nums):
+        zero=0
+        one=0
+        f={}
+        res=0
+
+        for i in range(len(nums)):
+            if nums[i]==0:
+                zero+=1
+            else:
+                one+=1
+            
+            diff=zero-one
+
+            if diff==0:
+                res=max(res,i+1)
+                continue
+            
+            if diff in f:
+                length=i-f[diff]
+                res=max(length,res)
+            
+            else:
+                f[diff]=i
+            
+        return res
+        
