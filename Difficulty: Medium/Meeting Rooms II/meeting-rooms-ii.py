2d96823714299e399a298e49230dbfc740fed678
Arghyadeep Deb <debarghyadeep4@gmail.com>
2026-09-06 13:13:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Meeting Rooms II/meeting-rooms-ii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minMeetingRooms(self, start, end):
+        # code here
+        start.sort()
+        end.sort()
+        room=0
+        res=0
+        i=0
+        j=0
+        while i<len(start) and j<len(end):
+            if start[i]<end[j]:
+                room+=1
+                res=max(res,room)
+                i+=1
+            
+            else:
+                room-=1
+                j+=1
+        
+        return res
+        
+        
